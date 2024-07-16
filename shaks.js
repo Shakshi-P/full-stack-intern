db5c5f457b2c3ac832e68d34d3eb3bcf006530bf
Shakshi-p <shakshishaks17@gmail.com>
2024-07-16 12:18:23 -0400

    changed by javascript

---
+++ shaks.js
@@ -0,0 +1,12 @@
+document.write("<table><thead><th>PLACE</th><th>RECIPES</th><th>RATING</th></thead>")
+for(var i=0;i<1;i++)
+{
+    for(var j=0;j<1;j++)
+    {
+        document.write("<tr><td>South India</td><td>Chicken briyani</td><td>4</td></tr>");
+        document.write("<tr><td>Mexico</td><td>Ranch chicken Tacos</td><td>4.5</td></tr>");
+        document.write("<tr><td>Asia</td><td>Lettuce wraps</td><td>4.2</td></tr>");
+    };
+
+};
+document.write("</table>");
